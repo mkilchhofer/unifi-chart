40dafa0bc5d4aeadd2310081b4661f9793abdf9c
Marco Maurer (-Kilchhofer) <mkilchhofer@users.noreply.github.com>
2024-10-14 22:38:54 +0200

    chore: Use correct email of the Renovate bot (#64)

--- .github/configs/renovate-config.js
+++ .github/configs/renovate-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     platform: 'github',
-    gitAuthor: 'renovate[bot] <renovate[bot]@users.noreply.github.com>',
+    gitAuthor: 'mkilchhofer-renovate[bot] <160166157+mkilchhofer-renovate[bot]@users.noreply.github.com>',
     autodiscover: false,
     allowPostUpgradeCommandTemplating: true,
     allowedPostUpgradeCommands: [".*"],
